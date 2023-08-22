4163f39e0003d1cb0fe070ed572f1c28537fa416
xmthms18 <xmthms@gmail.com>
2023-08-22 16:39:23 -0400

    commit

--- weatherapp/main_app/models.py
+++ weatherapp/main_app/models.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+
 class CustomUser(AbstractUser):
     # Add custom fields here if needed
     # For example:
     # birth_date = models.DateField(null=True, blank=True)
-    
-    def __str__(self):
-        return self.username
 
+    def __str__(self):
+      return self.username
--- weatherapp/main_app/views.py
+++ weatherapp/main_app/views.py
@@ -1,6 +1,58 @@
+import datetime 
+
+import requests
 from django.shortcuts import render
 
-# Define the home view
-def home(request):
-  # Include an .html file extension - unlike when rendering EJS templates
-  return render(request, 'home.html')
+
+def index(request):
+  APIKEY = open("API_KEY", "r").read()
+  current_weather_url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+  forecast_url = "http://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}"
+
+
+  if request.method == "POST":
+    city1 = request.POST['city1']
+    city2 = request.GET('city2', None)
+
+    weather_data1, daily_forecasts1 = fetch_weather_and_forcast(city1, APIKEY, current_weather_url, forecast_url)
+
+    if city2:
+      weather_data2, daily_forecasts2 = fetch_weather_and_forcast(city2, APIKEY, current_weather_url, forecast_url)
+    else: 
+      weather_data2, daily_forecasts2 = None, None
+
+      context = {
+        "weather_data1": weather_data1,
+        "daily_forecasts1": daily_forecasts1,
+        "weather_data2": weather_data2,
+        "daily_forecasts2": daily_forecasts2,
+      }
+
+      return render(request, "weather_app/index.html", context)
+
+  else:
+    return render(request, "weather_app/index.html")
+  
+
+def fetch_weather_and_forcast(city,api_key, current_weather_url,forecast_url):
+  response = requests.get(current_weather_url.format(city, api_key)).json()
+  lat,lon = response['coords']['lat'], response['coord']['lon']
+  forcast_response = requests.get(forecast_url.format(lat,lon,api_key)).json()
+
+  weather_data = {
+    "city": city,
+    "temperature": round(response['main']['temp'] - 273.15, 2),
+    "description": response['weather'][0]['description'],
+    "icon": response['weather'][0]['icon'],
+  }
+
+  daily_forecast = []
+  for  daily_data in forcast_response['daily'][:5]:
+    daily_forecast.append({
+      "day": datetime.datetime.fromtimestamp(daily_data['dt']).strftime("%A"),
+      "min_temp": round(daily_data['temp']['min'] - 273.15, 2),
+      "max_temp": round(daily_data['temp']['max'] - 273.15, 2),
+      "description": daily_data['weather'][0]['description'],
+      "icon": daily_data['weather'][0]['icon'],
+    })
+
